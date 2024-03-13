8d70f0ff3374ccbd1ce3d5b82a2da3cad3894ef3
TimShyong <timshyong@gmail.com>
2024-03-13 00:10:07 -0700

    Draw a player

--- frontend/src/classes/Player.ts
+++ frontend/src/classes/Player.ts
@@ -1,3 +1,26 @@
+import { PlayerProps } from "../interfaces/PlayerProps";
+import { Position } from "../interfaces/Position";
+import { Velocity } from "../interfaces/Velocity";
+
 export class Player {
-    
+    position: Position;
+    velocity: Velocity;
+    rotation: number;
+
+    constructor({position, velocity}: PlayerProps) {
+        this.position = position;
+        this.velocity = velocity;
+        this.rotation = 0;
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.beginPath();
+        ctx.moveTo(this.position.x + 30, this.position.y);
+        ctx.lineTo(this.position.x - 10, this.position.y - 10);
+        ctx.lineTo(this.position.x - 10, this.position.y + 10);
+        ctx.closePath();
+
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+    }
 }
--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import { CanvasProps } from '../interfaces/CanvasProps';
+import { Player } from '../classes/Player';
 
 const Canvas: React.FC<CanvasProps> = ({width, height}) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -7,11 +8,22 @@ const Canvas: React.FC<CanvasProps> = ({width, height}) => {
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas?.getContext('2d');
+
         if (canvas && ctx) {
+            // Fill background
             ctx.fillStyle = 'black';
             ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            // Create player
+            const player = new Player({
+                position: {x: width / 2, y: height /2},
+                velocity: {x: 0, y: 0}
+            });
+
+            player.draw(ctx);
+            console.log(player);
         }
-    }, []); 
+    }, [width, height]); // Will only rerender when screen size chages or twice on initial render because of React.StrictMode
 
     return ( 
         <canvas ref={canvasRef} width={width} height={height}/>
