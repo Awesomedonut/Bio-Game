6391de7b5336f8f1fdc2eaae991facc892bb50cb
Manpreet Kaur <manpreetkaursra11@gmail.com>
2024-03-17 23:08:31 -0700

    new user register router

--- backend/server.ts
+++ backend/server.ts
@@ -32,7 +32,7 @@ app.get('/', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data' });
   } 
 });
-app.post('/user/add', async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
     await gamemodel.addUser(req.body.username, req.body.email, req.body.password);
     res.json({ message: 'User has added successfully' });
@@ -41,6 +41,7 @@ app.post('/user/add', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data' });
   } 
 });
+
 app.get('/users', async (req, res) => {
   try{
     const recipes = await gamemodel.getAllUsers();
@@ -50,69 +51,7 @@ app.get('/users', async (req, res) => {
     res.status(500).json({ error: 'Failed to fetch data' });
   } 
 });
-/*
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
-
-app.get("/register", (req, res) => {
-  res.render("register.ejs");
-});
-
-app.post("/register", async (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-
-  try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (checkResult.rows.length > 0) {
-      res.send("Email already exists. Try logging in.");
-    } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedPassword = user.password;
-
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
-    } else {
-      res.send("User not found");
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-*/
 // app.post('/', (req: Request, res: Response) => {
 //   res.status(200).send('Hello World?');
 // })
