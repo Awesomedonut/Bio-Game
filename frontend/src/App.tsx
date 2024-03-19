8525961e423a3b5dee721e364cc83f21aca47ec2
Awesomedonut <Awesomedonut@users.noreply.github.com>
2024-03-19 15:22:45 -0700

    edit code to make it work when hosted on gcp app engine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/Dialogue" element={<Dialogue />} />
+        <Route path="/dialogue" element={<Dialogue />} />
 
       </Routes>
     </Router>
