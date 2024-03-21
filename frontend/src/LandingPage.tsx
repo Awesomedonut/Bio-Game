30553bbe76337973f8947e09c4ff471a49ce67d9
TimShyong <timshyong@gmail.com>
2024-03-20 19:49:43 -0700

    fixed minor ui bug

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -5,13 +5,14 @@ const LandingPage: React.FC = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <div className='flexbox'>
-      { !showLogin &&       
+    <div>
+      { !showLogin && <div className="flexbox">
         <div className='welcome'>
-          <h1>Welcome to the Game!</h1>
-          <p>Created by Group 2 for CMPT 372! We hope you enjoy! :D</p>
-          <button onClick={() => setShowLogin(true)}>Login to Play</button>
-        </div>
+            <h1>Welcome to the Game!</h1>
+            <p>Created by Group 2 for CMPT 372! We hope you enjoy! :D</p>
+            <button onClick={() => setShowLogin(true)}>Login to Play</button>
+          </div>
+      </div>      
       }
       {showLogin && <LoginModal onClose={() => setShowLogin(false)} />}
     </div>
