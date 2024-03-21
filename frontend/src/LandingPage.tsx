785db024d48a6d09b98edd1583e3efffbd05fc8e
TimShyong <timshyong@gmail.com>
2024-03-20 19:45:23 -0700

    stylised landing page

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -5,9 +5,9 @@ const LandingPage: React.FC = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <div>
+    <div className='flexbox'>
       { !showLogin &&       
-        <div>
+        <div className='welcome'>
           <h1>Welcome to the Game!</h1>
           <p>Created by Group 2 for CMPT 372! We hope you enjoy! :D</p>
           <button onClick={() => setShowLogin(true)}>Login to Play</button>
