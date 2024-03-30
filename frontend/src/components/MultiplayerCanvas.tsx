c4fe4110cefd3f76b022d675c65736dc9be2595e
TimShyong <timshyong@gmail.com>
2024-03-29 23:51:57 -0700

    Setup Canvas

--- frontend/src/components/MultiplayerCanvas.tsx
+++ frontend/src/components/MultiplayerCanvas.tsx
@@ -9,14 +9,36 @@ const MultiplayerCanvas: React.FC<CanvasProps> = ({width, height}) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext('2d');
+
+        if (ctx) {
+            start(ctx, width, height);
+        }
+
         // Listen for 'connect' event
         socket.on('connect', () => {
             console.log('Connected to server');
         })
     }, [socket])
 
+    function start(ctx: CanvasRenderingContext2D, width: number, height: number) {
+        let animationFrameID: number;
+
+        function animate() {
+            // Create animation loop
+            animationFrameID = window.requestAnimationFrame(animate);
+
+            // Clear background in frame
+            ctx.fillStyle = "rgb(255, 131, 122";
+            ctx.fillRect(0, 0, width, height);
+        }
+
+        animate();
+    }
+
     return ( 
-        <h1>Multiplayer Canvas Works!</h1>
+        <canvas ref={canvasRef} width={width} height={height}/>
     );
 }
  
