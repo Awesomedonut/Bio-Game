b7cde1ab108ce9ab956e0acf9430097b904dbc59
Manpreet Kaur <manpreetkaursra11@gmail.com>
2024-04-01 10:21:04 -0700

    commiting chnages before switching branch

--- backend/server.ts
+++ backend/server.ts
@@ -15,18 +15,18 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
-app.use(cookieParser());
-app.use(express.urlencoded({extended: true}))
+// app.use(cookieParser());
+// app.use(express.urlencoded({extended: true}))
 
-app.use(session({
-  name: 'nsession',
-  secret: 'session_secret_key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000*60*60 // Session expires in 1 hour
-  }
-}))
+// app.use(session({
+//   name: 'nsession',
+//   secret: 'session_secret_key',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     maxAge: 1000*60*60 // Session expires in 1 hour
+//   }
+// }))
 
 // for debugging
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -38,7 +38,16 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 //   res.status(200).send('Hello World?');
 // })
 
-app.get('/enemy/init', async (req, res) => {
+// app.get('/enemy/init', async (req, res) => {
+//   try {
+//     await gamemodel.init();
+//     res.send('Database has been initialized');
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     res.status(500).json({ error: 'Failed to fetch data' });
+//   } 
+// });
+app.get('/', async (req, res) => {
   try {
     await gamemodel.init();
     res.send('Database has been initialized');
@@ -60,7 +69,7 @@ app.get('/test', async (req, res) => {
 app.post('/register', async (req, res) => {
   try {
     await gamemodel.addUser(req.body.username, req.body.email, req.body.password);
-    (req.session as any).isLoggedIn = true;
+    //(req.session as any).isLoggedIn = true;
     res.json({ message: 'User has added successfully' });
   }catch (error) {
     console.error('Error fetching data:', error);
@@ -71,7 +80,7 @@ app.post('/register', async (req, res) => {
 app.get('/users', async (req, res) => {
   try {
     const allUsers = await gamemodel.getAllUsers();
-    (req.session as any).isLoggedIn = true;
+    //(req.session as any).isLoggedIn = true;
     res.json(allUsers);
   }catch (error) {
     console.error('Error fetching data:', error);
@@ -83,8 +92,8 @@ app.post('/login', async (req, res) => {
   try{
       const { username, password } = req.body;
       await gamemodel.getUser(username, password);
-      (req.session as any).isLoggedIn = true;
-      console.log("Logged In in /login post:" + (req.session as any).isLoggedIn)
+      // (req.session as any).isLoggedIn = true;
+      // console.log("Logged In in /login post:" + (req.session as any).isLoggedIn)
       res.json({ message: 'User logged in successfully' }); 
     }catch (error) {
       console.error('Error fetching data:', error);
@@ -92,18 +101,18 @@ app.post('/login', async (req, res) => {
     } 
 });
 
-app.use('/game', requireLogin, enemyRoutes);
-app.use('/game', requireLogin, playerRoutes);
+app.use('/game', enemyRoutes);
+app.use('/game', playerRoutes);
 
-function requireLogin(req: Request, res: Response, next: NextFunction) {
-  if ((req.session as any).isLoggedIn)  {
-    console.log("session logged in info:", (req.session as any).isLoggedIn)
-    next(); 
-  } else {
-    console.log("Unauthorized", (req.session as any).isLoggedIn);
-    res.status(401).json({ error: 'Unauthorized' });
-  }
-}
+// function requireLogin(req: Request, res: Response, next: NextFunction) {
+//   if ((req.session as any).isLoggedIn)  {
+//     console.log("session logged in info:", (req.session as any).isLoggedIn)
+//     next(); 
+//   } else {
+//     console.log("Unauthorized", (req.session as any).isLoggedIn);
+//     res.status(401).json({ error: 'Unauthorized' });
+//   }
+// }
 
 app.post('/dialogue', async (req: Request, res: Response) => {
   console.log(req.body);
@@ -120,16 +129,16 @@ app.post('/dialogue', async (req: Request, res: Response) => {
 
 })
 
-app.get('/logout', (req: Request, res: Response) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error('Error logging out:', err);
-      res.status(500).json({ error: 'Failed to log out' });
-      return;
-    }
-    res.json({ message: 'User logged out successfully' });
-  });
-});
+// app.get('/logout', (req: Request, res: Response) => {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       console.error('Error logging out:', err);
+//       res.status(500).json({ error: 'Failed to log out' });
+//       return;
+//     }
+//     res.json({ message: 'User logged out successfully' });
+//   });
+// });
 
 // Start the server
 const port = process.env.PORT || 3000;
