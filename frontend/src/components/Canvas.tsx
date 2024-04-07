2d888bb77aba23c5740c892a2b730ed9afb3f570
Awesomedonut <Awesomedonut@users.noreply.github.com>
2024-04-07 11:53:31 -0700

    adding instructions to asteroids level

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -13,6 +13,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 import Shop from '../components/Shop';
+import InstructionsPopup from './InstructionsPopup';
 
  const backendUri = "https://backend-dot-group-project372.uw.r.appspot.com/";
 //const backendUri ="http://localhost:4000"
@@ -23,6 +24,7 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
     const [enemiesData, setEnemies] = useState<EnemyInterface[]>([]);
     const [gameStarted, setGameStarted] = useState(false);
     const [showShop, setShowShop] = useState(false);
+    const [showInstructions, setShowInstructions] = useState(true);
 
     // Can't use React state for the game as react never rerenders past the initial useEffects
     let PLAYER_DAMAGE: MutableRefObject<number> = useRef(1);
@@ -87,6 +89,8 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
     };
 
     useEffect(() => {
+        if (!gameStarted) return;
+
         const fetchPlayer = async () => {
             const token = localStorage.getItem('token');
 
@@ -125,7 +129,7 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
             setGameStarted(true);
             console.log("GAME STARTED");
         }
-    }, [width, height, playerData, enemiesData]);
+    }, [width, height, playerData, enemiesData, gameStarted]);
 
     // BUG: Seems to call handleGameOver() multiple times at the end or during the game for no reason.
     //      Hopefully adding this flag will fix it, seems to work but unsure in long run.
@@ -459,8 +463,16 @@ const Canvas: React.FC<CanvasProps> = ({ width, height }) => {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    const startGame = () => {
+        setShowInstructions(false); // Hide instructions popup
+        setGameStarted(true); // Start the game
+    };
+
     return (
         <>
+                    {showInstructions && (
+                <InstructionsPopup onClose={startGame} />
+            )}
             <canvas ref={canvasRef} width={width} height={height} />
             {showShop &&
                 <Shop playerDamageRef={PLAYER_DAMAGE.current} playerSpeedRef={PLAYER_SPEED.current} playerProjCountRef={PLAYER_NUM_PROJECTILES.current}
