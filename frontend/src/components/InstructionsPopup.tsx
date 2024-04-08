72357865b73ae048ddaa4843bf7a1215dc42b5b1
Awesomedonut <Awesomedonut@users.noreply.github.com>
2024-04-08 08:59:38 -0700

    update instructions

--- frontend/src/components/InstructionsPopup.tsx
+++ frontend/src/components/InstructionsPopup.tsx
@@ -14,11 +14,11 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose, gameLeve
 
         // Determine instructions based on the game level
         if (gameLevel === 'flappy') {
-            instructionsText = "Here are the instructions for playing the game. Press Space to jump and avoid the obstacles.";
+            instructionsText = "Here are the instructions for playing the game. Press Space to jump and avoid the obstacles. As the bacteria, find a way into the human body. The obstacles represent mucous membranes, which produce mucus and contain specialized cells that help trap and remove bacteria. Your goal is to dodge these obstacles!";
         } else if (gameLevel === 'asteroid') {
-            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, move forward using the W key, and shoot using the Space key. Press P to open the shop; gain currency by killing enemies!";
+            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, move forward using the W key, and shoot using the Space key. Press P to open the shop; gain currency by killing enemies! Survive the innate immune response! The white blood cells kill bacteria by engulfing and breaking them down after contact; your goal is to avoid or shoot them. The green bullets represent the toxins that bacteria release to harm the human body.";
         } else if (gameLevel == 'multi') {
-            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, and move forward using the W key. Press P to open the shop; gain currency by killing enemies!";
+            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, and move forward using the W key. ";
         }else {
             instructionsText = "Invalid game level.";
         }
