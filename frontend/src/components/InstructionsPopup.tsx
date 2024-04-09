8a8ca7d56162ac011ffbeeb4e741b122059f958a
Awesomedonut <Awesomedonut@users.noreply.github.com>
2024-04-09 06:20:16 -0700

    fixing instructions

--- frontend/src/components/InstructionsPopup.tsx
+++ frontend/src/components/InstructionsPopup.tsx
@@ -14,11 +14,11 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose, gameLeve
 
         // Determine instructions based on the game level
         if (gameLevel === 'flappy') {
-            instructionsText = "Here are the instructions for playing the game. Press Space to jump and avoid the obstacles. As the bacteria, find a way into the human body. The obstacles represent mucous membranes, which produce mucus and contain specialized cells that help trap and remove bacteria. Your goal is to dodge these obstacles!";
+            instructionsText = "Press Space to jump and avoid the obstacles. As the bacteria, find a way into the human body. The obstacles represent mucous membranes, which produce mucus and contain specialized cells that help trap and remove bacteria. Your goal is to dodge these obstacles!";
         } else if (gameLevel === 'asteroid') {
-            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, move forward using the W key, and shoot using the Space key. Press P to open the shop; gain currency by killing enemies! Survive the innate immune response! The white blood cells kill bacteria by engulfing and breaking them down after contact; your goal is to avoid or shoot them. The green bullets represent the toxins that bacteria release to harm the human body.";
+            instructionsText = "Change directions using the A and D keys, move forward using the W key, and shoot using the Space key. Press P to open the shop; gain currency by killing enemies! Survive the innate immune response! The white blood cells kill bacteria by engulfing and breaking them down after contact; your goal is to avoid or shoot them. The green bullets represent the toxins that bacteria release to harm the human body.";
         } else if (gameLevel == 'multi') {
-            instructionsText = "Here are the instructions for playing the game. Change directions using the A and D keys, and move forward using the W key. ";
+            instructionsText = "Change directions using the A and D keys, and move forward using the W key. Avoid collision with the white blood cells and survive until the end!";
         }else {
             instructionsText = "Invalid game level.";
         }
@@ -27,7 +27,7 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose, gameLeve
         <div className="instructionsPopup">
             <div className="instructionsContainer">
                 <h2>Game Instructions</h2>
-                <p>Here are the instructions for playing the game. Press Space to jump and avoid the obstacles.</p>
+                <p>{instructionsText}</p>
                 <button onClick={onClose}>Start Game</button>
             </div>
         </div>
